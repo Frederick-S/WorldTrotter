ed1a93c19486343c6a4549f9772c2ac056af0c4b
Frederick-S <mao_xiaodan@hotmail.com>
2020-11-21 17:15:59 +0800

    viewDidLoad

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Fred on 2020/11/21.
+//
+
+import UIKit
+
+class MapViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
